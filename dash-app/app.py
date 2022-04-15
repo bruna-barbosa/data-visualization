0b95ae78f67b14bfca29e8e5e4e52ba0a872149f
Lucas-Correa <lukec.correa@gmail.com>
2022-04-15 18:17:10 +0100

    commit on 'main'

--- dash-app/app.py
+++ dash-app/app.py
@@ -26,7 +26,7 @@ import sort_dataframeby_monthorweek as sd
 
 # ********************* DATA PREPARATION *********************
 # Load data
-data = pd.read_csv("hotel_bookings.csv")
+data = pd.read_csv("dash-app/data/hotel_bookings.csv")
 
 # Format data for dashboard
 data["children"] = data["children"].fillna(0) # null chilrdren replace with 0
@@ -34,106 +34,85 @@ data["country"] = data["country"].fillna("Undefined") # null country replace wit
 data["agent"] = data["agent"].fillna(0)   # null agent replace with 0
 data["company"] = data["company"].fillna(0)  # null company replace with 0
 filter = (data.children == 0) & (data.adults == 0) & (data.babies == 0)
-data[filter]
 data = data[~filter]
 data["is_canceled"] = data["is_canceled"].replace({1:"Yes", 0:"No"})
 data["is_repeated_guest"] = data["is_repeated_guest"].replace({1:"Yes", 0:"No"})
 
+
+# ********************* DASH APP *********************
+app = dash.Dash(external_stylesheets=[dbc.themes.BOOTSTRAP])
 
 # Helper functions for dropdowns and slider
 def create_slider_marks(values):
     marks = {i: {'label': str(i)} for i in values}
     return marks
 
-# Functions
 def sort_month(df, column_name):
     # Function for sorting months
     return sd.Sort_Dataframeby_Month(df, column_name)
-
-
-# ********************* DASH APP *********************
-app = dash.Dash(external_stylesheets=[dbc.themes.BOOTSTRAP])
-
-# the styles for the main content position it to the right of the sidebar and
-# add some padding
-CONTENT_STYLE = {
-    "margin-left": "18rem",
-    "margin-right": "2rem",
-    "padding": "2rem 1rem",
-}
-
+    
 sidebar = html.Div([
-    html.Div([
-        html.H1("Hotel Bookings"),
-        html.P("In this project we intend to explore booking information for a city hotel and a resort hotel, comparing the differences between the choices made by guests with and without children along the years."),
-        # html.Img(src=app.get_asset_url("left_pane.png")),
-        html.Img(src="assets/icons/6.png"),
-        
-        html.Button(id='kids-button', children="Guests with Kids", n_clicks=0),
-        
-        html.Button(id='nokids-button', children="Guests without Kids", n_clicks=0),
-        
-        html.Button(id='all-button', children="All Guests", n_clicks=0),
-        ], id='left-container'),
+            html.H1("Hotel Bookings"),
+            html.P("In this project we intend to explore booking information for a city hotel and a resort hotel, comparing the differences between the choices made by guests with and without children along the years."),
+            # html.Img(src=app.get_asset_url("left_pane.png")),
+            html.Img(src="assets/icons/6.png")    
+            ],
+            id='left-container'
+            )
 
-], id='container')
-
-"""     html.Div([
-        html.Div(className='row', children=[
-            
-            html.Div(className='parent', children=[
-                dcc.Graph(id="barplot", className='plot'),
-                html.Div(className='spacer'),
-                dcc.Graph(id="map", className='plot'),
-                html.Div(className='spacer'),
-
-        html.Div(className='row', children=[            
-            html.Div(className='parent', children=[
-                dcc.Graph(id="piecharts", className='plot'),
-                html.Div(className='spacer'),
-                dcc.Graph(id="scatterplot", className='plot'),                
-            ])
-            ])
-            ])
-        ], id='visualisation'),   
-
-    ], id='right-container') """
-
-content = dbc.Container(
+content = html.Div([
+   
+    dbc.Container(
     [
         dbc.Row(
             [
-                dbc.Col(dcc.Graph(id="barplot"), md=4),
-                dbc.Col(dcc.Graph(id="map"), md=8),                
+                dbc.Col( daq.BooleanSwitch(
+                            id='kids_toggle',
+                            className='toggle',                
+                            label="With kids",
+                            labelPosition="top",
+                            on=False,
+                            color="#E9B000"
+                        ),),
+                dbc.Col(daq.BooleanSwitch(
+                            id='nokids_toggle',
+                            label="Without kids",
+                            labelPosition="top",
+                            on=True,
+                            color="#E9B000"                        
+                        ),),
+                dbc.Col( html.Button(id='all-button', children="All Guests", n_clicks=0)),
             ],
+            style={'padding':'.3rem', 'marginLeft':'1rem', 'boxShadow': '#e3e3e3 4px 4px 2px', 'border-radius': '10px', 'backgroundColor': 'white' },
             align="center",
         ),
 
         dbc.Row(
             [
-                dbc.Col(dcc.Graph(id="piecharts"), md=4),
-                dbc.Col(dcc.Graph(id="scatterplot"), md=8),                
+                dbc.Col(
+                    dcc.Graph(id="barplot"),
+                    style={'padding':'.3rem', 'marginLeft':'1rem', 'boxShadow': '#e3e3e3 4px 4px 2px', 'border-radius': '10px', 'backgroundColor': '#1D74C1' }
+                ),
+                dbc.Col(html.Img(src="assets/icons/4.png")),
+                dbc.Col(dcc.Graph(id="map")),                
             ],
+            #style={'padding':'.3rem', 'marginLeft':'1rem', 'boxShadow': '#e3e3e3 4px 4px 2px', 'border-radius': '10px', 'backgroundColor': 'white' },
             align="center",
         ),
+
+        # dbc.Row(
+        #     [
+        #         dbc.Col(dcc.Graph(id="piecharts")),
+        #         dbc.Col(dcc.Graph(id="scatterplot1")),                
+        #     ],
+        #     align="center",
+        # ),
     ],
-    fluid=True,
-)
+    #id='right-container',
+    fluid=True)
+    ])
 
-app.layout = html.Div([dcc.Location(id="url"), sidebar, content])
-
-""" @app.callback(
-    [Output(component_id='barplot', component_property='figure'),
-     Output(component_id='map', component_property='figure'),
-     Output(component_id='piecharts', component_property='figure'),
-     Output(component_id='scatterplot', component_property='figure')],
-    
-)  """ 
-
-
-    
 # Visual 1: Barplot
-
 
 # Visual 2: Map
 def map():
@@ -148,9 +127,13 @@ def map():
 
 # Visual 3: Pie charts
 
-
+@app.callback(
+    Output(component_id='scatterplot1', component_property='figure'),
+    [Input(component_id='kids_toggle', component_property='on'),
+     Input(component_id='nokids_toggle', component_property='on')]
+) 
 # Visual 4: Scatterplot
-def scatterplot():
+def scatterplot(kids,no_kids):
     # Function for creating a Scatterplot
     
     # Guests without children
@@ -184,10 +167,10 @@ def scatterplot():
                   yaxis=dict(title='Number of guests')
                   )
 
-    scatterplot = go.Figure(data=month_data, layout=month_layout)
+    return go.Figure(data=month_data, layout=month_layout)  
 
-    return scatterplot  
 
+app.layout = html.Div([dcc.Location(id="url"), sidebar, content])
 
 if __name__ == '__main__':
     app.run_server(debug=True)
